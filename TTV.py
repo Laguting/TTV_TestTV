e9362219eb741a5aaef9edfd1c5000d1b7fa4a40
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-14 15:20:27 +0800

    Assigning the attributes of TV 1 and 2 as well as allowing it to be displayed

--- TTV.py
+++ TTV.py
@@ -69,11 +69,51 @@ if ask_usr.upper() == "Y":
     
     #Test TV driver
     # Assign TV 1 and TV 2' attributes (channel and volume)
-    tv_1 = TV(0,0,True) # TV 1
+    tv_1 = TV(0,0,0) # TV 1
+    tv_1.power_on()
     tv_1.set_channel(30)
     tv_1.set_volume(3)
-    # TV 2
+    tv_2 = TV(0,0,0) # TV 2
+    tv_2.power_on()
+    tv_2.set_channel(3)
+    tv_2.set_volume(2)
     # Display the results
-    print("The TV 1's channel is", tv_1.get_channel(), "and volume is ", tv_1.get_volume())
+    print("\33[45m\33[1mThe TV 1's channel is", tv_1.get_channel(), "\33[45m\33[1m and volume is ", tv_1.get_volume(), "\33[0m")
+    print("\33[45m\33[1mThe TV 1's channel is ", tv_2.get_channel(), "\33[45m\33[1m and volume is ", tv_2.get_volume(), "\33[0m")
+    print()
+    print("⬜"* 88)
     # Change channel, volume, turn off tv 1, 2, or both
+    while True:
+        try:
+            usr_chc = input("\n\33[1m\33[92m If you don't want to change anything press '0' ; If you want to change Channel for TV 1 press '1' ; If you want to change Channel for TV 2 press '2' ; If you want to change volume for TV 1 press '3' ; If you want to change volume for TV 2 press '4' ; If you want turn TV 1 off press '5' ; If you want turn TV 1 off press '6': \33[0m")
+            if usr_chc not in ["0","1","2","3","4","5","6"]:
+                raise ValueError
+        except ValueError:
+            print("\n\33[1m\33[31mInvalid Operation. Try again.\33[0m")
+            continue
+        if usr_chc == "1":
+                new_channel = int(input("\n\33[1m\33[7mEnter new channel: \33[0m"))
+                tv_1.set_channel(new_channel)
+                print("\n\33[45m\33[1mThe TV 1's channel is", tv_1.get_channel(), "\33[45m\33[1m and volume is ", tv_1.get_volume(), "\33[0m")
+        if usr_chc == "2":
+                new_channel = int(input("\n\33[1m\33[7mEnter new channel: \33[0m"))
+                tv_2.set_channel(new_channel)
+                print("\n\33[45m\33[1mThe TV 2's channel is ", tv_2.get_channel(), "\33[45m\33[1m and volume is ", tv_2.get_volume(), "\33[0m")
+        if usr_chc == "3":
+                new_volume = int(input("\n\33[1m\33[7mEnter new volume level: \33[0m"))
+                tv_1.set_volume(new_volume)
+                print("\n\33[45m\33[1mThe TV 1's channel is ", tv_1.get_channel(), "\33[45m\33[1m and volume is ", tv_1.get_volume(), "\33[0m")
+        if usr_chc == "4":
+                new_volume = int(input("\n\33[1m\33[7mEnter new volume level: \33[0m"))
+                tv_2.set_channel(new_volume)
+                print("\n\33[45m\33[1mThe TV 2's channel is ", tv_2.get_channel(), "\33[45m\33[1m and volume is ", tv_2.get_volume(), "\33[0m")
+        if usr_chc == "5":
+                tv_1.power_off()
+                print("\n\33[45m\33[1mTV 1 is turned off")
+        if usr_chc == "6":
+                tv_2.power_off()
+                print("\n\33[45m\33[1mTV 2 is turned off")
+        else:
+            usr_chc == "0"
+            break
     # Close the TV
