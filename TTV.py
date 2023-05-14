351982d765c1b0b187a43dea3e7a6e0249333f79
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-14 14:34:25 +0800

    The function for changing volumes

--- TTV.py
+++ TTV.py
@@ -5,6 +5,10 @@
 # tv1's channel is 30 and volume level is 3
 # tv2's channel is 3 and volume level is 2
 
+from pyfiglet import Figlet
+from termcolor import colored
+from tqdm import tqdm 
+import time
 print()
 print("⬜"* 88)
 user_name = input("\n\n\33[1m\33[33mInput your name here ✍️  :\33[0m ")
@@ -12,18 +16,14 @@ ask_usr = input("\n\33[1m\33[33mHi " + str(user_name) + "\33[1m\33[33m, would yo
 if ask_usr.upper() == "Y":
 # Loading bar
     print()
-    from tqdm import tqdm 
-    import time
     for i in tqdm (range (100), desc="Opening...\U0001F973"):
         time.sleep(0.05)
         pass
 # Intro to the brand of the TV
-    from pyfiglet import Figlet
-    from termcolor import colored
     intro_brand = Figlet( font = "doh", justify = "right")
     print()
     print("⬜"* 88)
-    print(colored(intro_brand.renderText("AETH3R"), "cyan"))
+    print(colored(intro_brand.renderText("   AETH3R"), "cyan"))
     # Create class for TV
     class TV():
     # Channel, Volume, and Power is the parameters- default of the TV
@@ -56,18 +56,24 @@ if ask_usr.upper() == "Y":
         def channels_up(self):# Channel increases by 1
             if self.power_on and self.channel < 120:
                 self.channel + 1
-        def channels_up(self):# Channel decreases by 1
+        def channels_down(self):# Channel decreases by 1
             if self.power_on and self.channel > 1:
                 self.channel - 1
     # Change Volume
-    # Volume increases by 1
-    # Volume decreases by 1
+        def volume_up(self):# Volume increases by 1
+            if self.power_on and self.volume < 7:
+                self.volume + 1
+        def volume_down(self):# Volume decreases by 1
+            if self.power_on and self.volume > 1:
+                self.volume - 1
     
     #Test TV driver
     # Assign TV 1 and TV 2' attributes (channel and volume)
-    tv_1 = TV(30,0,True)
+    tv_1 = TV(0,0,True) # TV 1
+    tv_1.set_channel(30)
     tv_1.set_volume(3)
+    # TV 2
     # Display the results
-    print("The TV 1's channel is ", tv_1.get_channel(), " and volume is ", tv_1.get_volume())
+    print("The TV 1's channel is", tv_1.get_channel(), "and volume is ", tv_1.get_volume())
+    # Change channel, volume, turn off tv 1, 2, or both
     # Close the TV
-
