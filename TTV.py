7bf2cf690ab24de6927aa952a6fc0a4f0af91b09
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-14 11:41:44 +0800

    Tentative psuedocodes base on my analyzation and understanding

--- TTV.py
+++ TTV.py
@@ -4,3 +4,25 @@
 # objects from Class TV and will produce the following output:
 # tv1's channel is 30 and volume level is 3
 # tv2's channel is 3 and volume level is 2
+
+# Intro to the brand of the TV
+
+# Create class for TV
+# Channel, Volume, and Power is the parameters- default of the TV
+# Set up the power and use boolean where: off = false and on = true
+# Power on
+# Power off
+# Return the channel
+# Set a new channel
+# Return volume
+# Set a new volume
+# Change Channels
+# Channel increases by 1
+# Channel decreases by 1
+# Change Volume
+# Volume increases by 1
+# Volume decreases by 1
+# Assign TV 1 and TV 2' attributes (channel and volume)
+# Display the results
+
+# Close the TV
