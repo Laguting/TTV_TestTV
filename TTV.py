603940be704f96a32c38c98da21dd954cbe58818
Laguting <lagutingmariconjaneg@gmail.com>
2023-05-14 17:35:43 +0800

    Using while loop to allow modifications in the channels and volume

--- TTV.py
+++ TTV.py
@@ -95,25 +95,35 @@ if ask_usr.upper() == "Y":
                 new_channel = int(input("\n\33[1m\33[7mEnter new channel: \33[0m"))
                 tv_1.set_channel(new_channel)
                 print("\n\33[45m\33[1mThe TV 1's channel is", tv_1.get_channel(), "\33[45m\33[1m and volume is ", tv_1.get_volume(), "\33[0m")
+                continue
         if usr_chc == "2":
                 new_channel = int(input("\n\33[1m\33[7mEnter new channel: \33[0m"))
                 tv_2.set_channel(new_channel)
                 print("\n\33[45m\33[1mThe TV 2's channel is ", tv_2.get_channel(), "\33[45m\33[1m and volume is ", tv_2.get_volume(), "\33[0m")
+                continue
         if usr_chc == "3":
                 new_volume = int(input("\n\33[1m\33[7mEnter new volume level: \33[0m"))
                 tv_1.set_volume(new_volume)
                 print("\n\33[45m\33[1mThe TV 1's channel is ", tv_1.get_channel(), "\33[45m\33[1m and volume is ", tv_1.get_volume(), "\33[0m")
+                continue
         if usr_chc == "4":
                 new_volume = int(input("\n\33[1m\33[7mEnter new volume level: \33[0m"))
                 tv_2.set_channel(new_volume)
                 print("\n\33[45m\33[1mThe TV 2's channel is ", tv_2.get_channel(), "\33[45m\33[1m and volume is ", tv_2.get_volume(), "\33[0m")
+                continue
         if usr_chc == "5":
                 tv_1.power_off()
-                print("\n\33[45m\33[1mTV 1 is turned off")
+                print("\n\33[45m\33[1mTV 1 is turned off\33[0m")
+                continue
         if usr_chc == "6":
                 tv_2.power_off()
-                print("\n\33[45m\33[1mTV 2 is turned off")
+                print("\n\33[45m\33[1mTV 2 is turned off\33[0m")
+                continue
         else:
             usr_chc == "0"
+            tv_exit = Figlet( font = "slant", justify = "right")
+            print ()
+            print(colored(tv_exit.renderText("                               AETH3R.........."), "red"))
+            print("\33[1m\33[3m                                                                                     OFF\33[0m\n")
             break
     # Close the TV
